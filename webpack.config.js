041e2d8fe52d6ec1eb69c46821127fe1c729c7e0
Sirius CMa <karpunaa@yandex.ru>
2023-03-12 13:16:14 +0300

    *перенос папки images*
    *исправление css*

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,10 @@ module.exports = {
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'), // : путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    compress: true,
+    port: 8080,
 
-    open: true // сайт будет открываться сам при запуске npm run dev
+    open: true
   },
   module: {
     rules: [
