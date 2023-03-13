abf3bbf9787e17e0c60ba5eb3889d5de090bd453
Sirius CMa <karpunaa@yandex.ru>
2023-03-13 10:43:19 +0300

    *исправлены ошибки в валидации*

--- src/components/validate.js
+++ src/components/validate.js
@@ -2,9 +2,9 @@ export const popupElements = {
   form: '.form',
   input: '.popup__input',
   saveButton: '.popup__save-button',
-  disableModifier: '.popup__save-button_disabled',
-  textError: '.popup__input-error',
-  inputModifier: '.popup__input_error'
+  disableModifier: 'popup__save-button_disabled',
+  inputErrorModifier: 'popup__input_error', // красная линия
+  textErrorModifier: 'popup__input-error_active' // текст ошибки
 }
 
 
@@ -33,24 +33,22 @@ function initForms(popupElements) {
 
 const showInputError = (element, formPopup, popupElements) => {
   const err = formPopup.querySelector(`.${element.id}-error`)
-  element.classList.add(popupElements.textError);
-  err.classList.add(popupElements.inputModifier)
+  element.classList.add(popupElements.inputErrorModifier);
+  err.classList.add(popupElements.textErrorModifier)
   err.textContent = element.validationMessage
 };
 
 const hideInputError = (element, formPopup, popupElements) => {
   const err = formPopup.querySelector(`.${element.id}-error`)
-  element.classList.remove(popupElements.textError)
-  err.classList.remove(popupElements.inputModifier)
+  element.classList.remove(popupElements.inputErrorModifier)
+  err.classList.remove(popupElements.textErrorModifier)
 };
 
 const isValid = (input, formPopup, popupElements) => {
   console.log(`Осталось ${200 - input.value.length} символов`)
   input.validity.patternMismatch
     ? input.setCustomValidity(input.dataset.errorMessageType)//console.log('не правильно')
-    : input.validity.tooShort
-      ? input.setCustomValidity(input.dataset.errorMessageLength)
-      : input.setCustomValidity('')//console.log('правильно ')
+    : input.setCustomValidity('')//console.log('правильно ')
 
   !input.validity.valid
     ? showInputError(input, formPopup, popupElements)
@@ -71,8 +69,13 @@ export function switchingSaveButton(inputs, saveBtn, popupElements) {
 
 export function cleareInputs(popupElements) {
   setTimeout(() => document.querySelectorAll(popupElements.input)
-    .forEach(element => element.value = ''), 1000)
-}
+    .forEach(element => {
+      element.value = ''
+      const formPopup = element.closest('.form')
+      hideInputError(element, formPopup, popupElements)
+    }),
+    1000)
+};
 
 
 
