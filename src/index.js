9eaf6b51617c3daa55aa2a5056511f8dcc8c4c2e
Sirius CMa <karpunaa@yandex.ru>
2023-03-19 15:06:22 +0300

    *current saving*

--- src/index.js
+++ src/index.js
@@ -17,9 +17,10 @@ import { initialCards } from './components/datacard.js';
 import { closePopup, openPopup } from './components/modal.js'
 import { addElement, createElement } from './components/card.js';
 import { switchingSaveButton, initForms, prepareForm } from './components/validate.js';
-import { getContentServer } from './components/api.js';
-import { loadImage } from './components/utils.js';
+import { getContentServer, getDataProfile, saveAvatarProfile } from './components/api.js';
+import { loadImage, checkButton } from './components/utils.js';
 
+export const idProfile = {};
 export const popupElements = {
   form: '.form',
   input: '.popup__input',
@@ -36,10 +37,14 @@ export const profile = document.querySelector('.profile');
 const popupFullsizeImage = document.querySelector('.popup-photo-fullsize');
 const popupAddingPlace = document.querySelector('.popup-add-place');
 const popupEditingProfile = document.querySelector('.popup-edit-profile');
+const popupEditingAvatar = document.querySelector('.popup-edit-avatar')
+const popupErrorAvatar = document.querySelector('.popup-error-avatar')
+const popupDeleteCard = document.querySelector('.popup-delete-card')
 
 // : формы
 const formEditingProfile = document.getElementById('edit-profile');
 const formAddingPlace = document.getElementById('add-place');
+const formEditingAvatar = document.getElementById('edit-avatar');
 const saveBtnAddPlace = formAddingPlace.querySelector(popupElements.saveButton)
 
 
@@ -141,7 +146,33 @@ closingButtons.forEach(closingBtn => {
   })
 });
 
+// : кнопка редактирования аватара
+const editingAvatarButton = profile.querySelector('.profile__button-edit-avatar')
+editingAvatarButton.addEventListener('click', () => {
+  prepareForm(formEditingAvatar, popupElements);
+  openPopup(popupEditingAvatar)
+})
 
+
+
+const linkFormAvatar = document.getElementById('input-link-avatar')
+
+// : кнопка сохранения аватара
+formEditingAvatar.addEventListener('submit', (evt) => {
+
+  (evt.stopPropagation(),
+    evt.preventDefault(),
+    console.log(linkFormAvatar.value),
+    editAvatar(linkFormAvatar.value, evt))
+
+})
+
+
+
+// : сохранения данных профиля
+formEditingProfile.addEventListener('submit', (evt) => {
+  saveProfile(evt);
+});
 
 
 
@@ -149,6 +180,9 @@ closingButtons.forEach(closingBtn => {
 // initialCards.forEach(card => {
 //   addElement(createElement(card.name, card.link));
 // });
+
+
+// : загрузка картинок
 
 export function initialCard() {
   getContentServer()
@@ -165,10 +199,80 @@ export function initialCard() {
 }
 
 
-initialCard();
+
+
+// : загрузка и создание профиля
+
+export function initialProfile() {
+  getDataProfile()
+    .then((res) => {
+      // idProfile._id = res._id
+      createProfile(res)
+    })
+    .catch(err => console.log(err))
+};
+
+
+export function editAvatar(link, evt) {
+  console.log(44444444);
+  checkButton(evt, 'Сохраняю...')
+  loadImage(link)
+    .then(() => {
+      saveAvatarProfile(link)
+        .then(res => {
+          createProfile(res)
+          closePopup(popupEditingAvatar)
+          checkButton(evt, 'Сохранить', 1000)
+        })
+        .catch(err => console.log(err))
+
+    })
+    .catch(() => {
+      closePopup(popupEditingAvatar)
+      openPopup(popupErrorAvatar)
+      checkButton(evt, 'Сохранить', 1000)
+    })
+
+};
+
+export function editProfile(name, about, evt) {
+  checkButton(evt, 'Сохраняю...')
+  saveDataProfile(name, about)
+    .then(res => {
+      createProfile(res)
+      checkButton(evt, 'Сохранить', 1000)
+    })
+    .catch(err => {
+      console.log(err)
+      checkButton(evt, 'Сохранить', 1000)
+    })
+  // cleareInputs()
+}
+
+
+
+
+const avatarProfile = document.querySelector('.profile__avatar')
+
+
+export function createProfile(data) {
+  console.log('Профиль -', data)
+  idProfile._id = data._id
+  nameProfile.textContent = data.name;
+  professionProfile.textContent = data.about;
+  avatarProfile.src = data.avatar;
+  avatarProfile.alt = data.name
+  closePopup(popupEditingProfile);
+}
 
 
 
 
 
+
+
+// : активация данных
+
+initialProfile();
+// initialCard();
 initForms(popupElements);
